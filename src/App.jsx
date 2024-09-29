b27229d9b8dc37a059e6f5f55231ee00ece8ade3
Erick <erickacevedor@gmail.com>
2024-09-29 11:16:01 -0500

    React scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,36 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+
+import Nav from './components/Nav/Nav'
+import Footer from './components/Footer/Footer'
+
 import Home from './pages/Home'
 import About from './pages/About/About'
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About Us</Link>
-            </li>
-          </ul>
-        </nav>
+      <>
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
+        {/* Footer */}
+
+        <Nav />
+
+
+
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+
+
+        {/* Footer */}
+        <Footer />
+
+      </>
     </Router>
   )
 }
