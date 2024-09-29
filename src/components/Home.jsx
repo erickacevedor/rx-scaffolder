5a9b1a0593539adba8d15c26c2bb1709b885a47a
Erick <erickacevedor@gmail.com>
2024-09-29 11:04:51 -0500

    React scaffolder

---
+++ src/components/Home.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function Home() {
+    const [count, setCount] = React.useState(0)
+
+    return (
+        <>
+            <h1>Home</h1>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h2>Vite + React</h2>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/components/Home.jsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    )
+}
+
+export default Home
